3016be1b587381e03965a90eca603b914c8eafb7
Philip Phlek <pphlek@gmail.com>
2024-12-12 09:02:55 -0500

    solved 2024 day 11

---
+++ 2024/day_11/part_2.ts
@@ -0,0 +1,76 @@
+// https://adventofcode.com/2024/day/11
+
+import * as fs from 'fs'
+
+function transformStonesHelper(stone: number): number[] {
+  if (stone === 0) return [1]
+
+  const stoneStr = String(stone)
+  if (stoneStr.length % 2 === 0) {
+    return [
+      +stoneStr.substring(0, stoneStr.length / 2),
+      +stoneStr.substring(stoneStr.length / 2),
+    ]
+  }
+
+  return [stone * 2024]
+}
+
+type StoneTrack = {
+  stone: number
+  count: number
+}
+
+function transformStones(
+  stoneTracker: Record<number, number>,
+  blinks: number
+): Record<number, number> {
+  let latestStoneTracker = {...stoneTracker}
+
+  for (let i = 0; i < blinks; i++) {
+    const currStoneTracker = {...latestStoneTracker}
+    latestStoneTracker = {}
+
+    Object.entries(currStoneTracker).forEach(([stone, count]) => {
+      const [stone1, stone2] = transformStonesHelper(+stone)
+      if (!latestStoneTracker[stone1]) {
+        latestStoneTracker[stone1] = count
+      } else {
+        latestStoneTracker[stone1] += count
+      }
+
+      if (!isNaN(stone2)) {
+        if (!latestStoneTracker[stone2]) {
+          latestStoneTracker[stone2] = count
+        } else {
+          latestStoneTracker[stone2] += count
+        }
+      }
+    })
+  }
+
+  return latestStoneTracker
+}
+
+function solve(file: string) {
+  const lines = fs.readFileSync(file, 'utf-8').split(/[\n\r]+/)
+
+  const stoneTracker: Record<number, number> = {}
+  const stones = lines[0].split(' ').map((stone) => +stone)
+
+  stones.forEach((stone) => {
+    if (!stoneTracker[stone]) {
+      stoneTracker[stone] = 1
+    } else {
+      stoneTracker[stone]++
+    }
+  })
+
+  const latestStoneTracker = transformStones(stoneTracker, 75)
+  return Object.values(latestStoneTracker).reduce(
+    (sum, count) => sum + count,
+    0
+  )
+}
+
+console.log(solve('input.txt'))
